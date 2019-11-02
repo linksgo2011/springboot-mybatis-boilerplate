e45bc99e889f714e36152440b4d06067106755b3
linksgo2011 <120377843@qq.com>
2019-11-02 23:48:35 +0800

    add basic swagger ui

---
+++ src/main/java/boilerplate/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package boilerplate.config;
+
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("open-api")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Boilerplate API")
+                .description("API build on boilerplate API")
+                .termsOfServiceUrl("https://github.com/linksgo2011/springboot-mybatis-boilerplate")
+                .contact(new Contact("printf", "http://printf.cn", "120377843@qq.com")).license("JavaInUse License")
+                .licenseUrl("120377843@qq.com").version("1.0").build();
+    }
+}
